669fa0795becdd205510644d997660298985581f
Pavan Ambali <pavanambali165@gmail.com>
2022-06-30 01:06:09 +0200

    Set up and Delivery 1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Best practices of data science from the industry while developing a COVID-19 analysis prototype.',
+    author='Pavan Ambali',
+    license='MIT',
+)
